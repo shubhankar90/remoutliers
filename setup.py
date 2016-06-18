1d0f9183322542a0beaa854149833c221585b44a
shubhankar90 <shubhankar90@gmail.com>
2016-06-18 14:06:05 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 18 13:21:02 2016
+
+@author: alienware
+"""
+
+from setuptools import setup
+
+setup(name='remoutliers',
+      version='0.1',
+      description='Removing outliers from pandas dataframe',
+      url='http://github.com/shubhankar90/remoutliers',
+      author='Shubhankar Mitra',
+      author_email='shubhankar90@gmail.com',
+      license='GNU GENERAL PUBLIC LICENSE',
+      packages=['remoutliers'],
+      zip_safe=False)
